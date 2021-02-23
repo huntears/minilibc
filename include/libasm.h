5a49f08f936095460fc212d24de5fcafdec7a6ad
Huntears <dev@huntears.com>
2021-02-23 10:30:32 +0100

    Add strlen

--- include/libasm.h
+++ include/libasm.h
@@ -11,7 +11,7 @@
 #include <stddef.h>
 
 size_t strlen(const char *s);
-char *strchr(const char *s);
+char *strchr(const char *s, int c);
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 int strcmp(const char *s1, const char *s2);
---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** asm
+** File description:
+** main
+*/
+
+#include "libasm.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Length of \"Issou\" : %d\n", strlen("Issou"));
+    return 0;
+}
